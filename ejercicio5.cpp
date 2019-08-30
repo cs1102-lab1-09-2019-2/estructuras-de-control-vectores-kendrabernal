710b77b3a1174dfeab87e0ab4f8b693153005bba
kendrabernal <54277329+kendrabernal@users.noreply.github.com>
2019-08-30 14:11:31 -0500

    Subir ejercicio5.cpp
    Terminado

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -30,20 +30,21 @@ int main() {
     delta=pow(b,2)-4*a*c;
     if (delta>0){
      x=-b+(sqrt(delta))/(2*a);
-    y=-b-(sqrt(delta))/(2*a);
+     y=-b-(sqrt(delta))/(2*a);
   }
   if (a==0 and b==0)
-    cout<<"No tiene solucion";
+   cout<<"No tiene solucion";
   else 
-    if(a==0)
-      cout<<-c/b;
-    else 
-      if (delta<0)
-        cout<<"No tiene solucion";
-      else
-        cout<<"La ecuacion tiene solucion";
+   if(a==0)
+     cout<<-c/b;
+   else 
+     if (delta<0)
+       cout<<"No tiene solucion";
+     else
+       cout<<"La ecuacion tiene solucion";
     
-  return 0;
+  
+ return 0;
  
  
 
