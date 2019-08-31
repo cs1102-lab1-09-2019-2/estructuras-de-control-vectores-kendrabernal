07d539f809e95b9d5312a0a05e6be5d150564dbe
kendrabernal <54277329+kendrabernal@users.noreply.github.com>
2019-08-30 22:57:03 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
 
 int moda(vector<int> &lista);
@@ -5,14 +9,45 @@ vector<int> leerConsole();
 
 vector<int> leerConsola() {
     //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+    float calcularModa(vector<int> &lista) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < lista.size(); i++) {
+        map[lista[i]]++;
+    }
+    int moda, max = -10;
+    for (auto &it : map) {
+        if (it.second > max) {
+            max = it.second;
+            moda = it.first;
+        }
+    }
+    return moda;
 }
+}
+
 
 int moda(vector<int> &lista) {
     /*
     La moda es el elemento que mas se repite en la lista de elementos
      */
+    float calcularModa(vector<int> &lista) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < lista.size(); i++) {
+        map[lista[i]]++;
+    }
+    int moda, max = -10;
+    for (auto &it : map) {
+        if (it.second > max) {
+            max = it.second;
+            moda = it.first;
+        }
+    }
+    return moda;
+}
+    
     return null;
 }
+
 
 
 int main() {
